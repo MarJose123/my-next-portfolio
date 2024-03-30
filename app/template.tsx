b88d35b369a36f75e0b85edef9bfe800d079c12e
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-03-30 19:47:11 +0800

    feat: Add social media links to navigation
    Add social media links to the navigation bar, including LinkedIn, Twitter, GitHub, and a dark mode toggle.
    
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- app/template.tsx
+++ app/template.tsx
@@ -16,30 +16,79 @@ export default function Template({children}: { children: React.ReactNode }) {
         <div className="flex w-full max-h-dvh py-10">
             <div className="flex w-full flex-col px-72">
                 <nav className='flex w-full'>
-                    <div className='flex bg-transparent ml-auto'>
-                        <div
-                            className='block dark:hidden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110'
-                            onClick={() => {
-                            localStorage.theme = 'dark'
-                            setTheme('dark');
-                        }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                 strokeWidth={1.5}
-                                 stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                      d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z"/>
+                    <div className='flex bg-transparent ml-auto gap-4'>
+                        <a
+                            href='https://www.linkedin.com/in/josie-darang'
+                            target='_blank'
+                            className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110'
+                        >
+                            <svg
+                                className="h-6 w-6 dark:fill-slate-300"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                            >
+                                <path
+                                    d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z"/>
                             </svg>
-                        </div>
-                        <div className='hidden dark:block' onClick={() => {
-                            localStorage.theme = 'light'
-                            setTheme('light')
-                        }}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                 strokeWidth={1.5}
-                                 stroke="currentColor" className="w-6 h-6 dark:stroke-slate-50">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                      d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"/>
+                        </a>
+                        <a
+                            href='#'
+                            target='_blank'
+                            className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110'
+                        >
+                            <svg
+                                className="h-7 w-7 dark:fill-slate-300"
+                                fill="currentColor"
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                            >
+                                <path
+                                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"/>
                             </svg>
+                        </a>
+                        <a
+                            href='https://github.com/marjose123'
+                            target='_blank'
+                            className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110'
+                        >
+                            <svg viewBox="0 0 16 16"
+                                 className="w-6 h-6 dark:fill-slate-300"
+                                 fill="currentColor"
+                                 aria-hidden="true">
+                                <path
+                                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+                            </svg>
+                        </a>
+                        <div className='flex items-center border-l border-slate-300 ml-3 pl-6 dark:border-slate-800'>
+                            <div
+                                className='block dark:hidden transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 cursor-pointer'
+                                onClick={() => {
+                                    localStorage.theme = 'dark'
+                                    setTheme('dark');
+                                }}>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="w-7 h-7 dark:stroke-slate-50">
+                                    <path fillRule="evenodd"
+                                          d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                                          clipRule="evenodd"/>
+                                </svg>
+                            </div>
+                            <div
+                                className='hidden dark:block transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 cursor-pointer'
+                                onClick={() => {
+                                    localStorage.theme = 'light'
+                                    setTheme('light')
+                                }}>
+
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                     className="w-7 h-7 dark:stroke-slate-50  dark:fill-slate-300">
+                                    <path
+                                        d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z"/>
+                                </svg>
+
+
+                            </div>
                         </div>
                     </div>
                 </nav>
