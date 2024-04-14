cea6736e3a479eea7dd6ceb25f68c1bd91efef1c
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2024-04-14 12:33:52 +0800

    removed comment
    Signed-off-by: MarJose123 <18107626+MarJose123@users.noreply.github.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,6 @@ export const getServerSideProps = (async () => {
 export default function Page({
   data,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
-  // return data;
   return (
     <>
       <Head>
